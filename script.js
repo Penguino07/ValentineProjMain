3d3fe928bca3326d4ca662380262365268311d51
Penguino07 <Gameplaymaster1114@gmail.com>
2025-02-09 22:26:14 -0800

    4th commit?

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const mizuki = document.querySelector(".mizuki img");
+
+document.addEventListener("DOMContentLoaded", function() {
+    mizuki.style.display = "none";
+})
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         const giftBox = document.querySelector(".box img");
@@ -22,17 +30,65 @@ document.addEventListener("DOMContentLoaded", function() {
     box.addEventListener("click", function() {
         
         // Check if the box has been clicked 3 times
-        if (clickCount !== 3) {
+        if (clickCount < 3) {
             triggerShakeAnimation();
         }else {
             console.log("Box clicked 3 times!");
             // Add your event logic here
             // For example, you can change the image or display a message
             box.src = "pictures/Designer2.png"; // Change the image
-            
+            disappear();
         }
         console.log("Box clicked " + clickCount + " times");
         clickCount++;
         
     });
+
+    function disappear() {
+        mizuki.style.display = "block";
+        box.style.animation = "fadeOut 2s linear, zoomIn 2s linear";
+        setTimeout(function() {
+            
+            box.style.display = "none";
+            box.style.animation = "";
+            change();
+        }, 2000); // 3-second delay
+    }
+    const background = document.querySelector("body");
+    const txt1 = document.querySelector("h1");
+    const txt2 = document.querySelector(".evilbottom marquee");
+    const txt3 = document.querySelector(".intro h2");
+    function change(){
+        
+        console.log(txt1);
+        mizuki.style.animation = "moveMizuki 2s linear";
+        console.log("Mizuki is moving");
+        setTimeout(function() {
+            background.style.backgroundColor = "pink";
+            mizuki.style.animation = "";
+            mizuki.style.top = "25%";
+            txt1.innerHTML = "========================HAPPY VALENTINES DAY!========================";
+            txt2.innerHTML = "========================HAPPY VALENTINES DAY!========================";
+            txt3.innerHTML = "";
+            speech();
+        }, 2000);
+    }
+
+    function speech(){
+        const speech = document.querySelector(".speechBubble img");
+        const txt = document.querySelector(".talk h2");
+        console.log(txt);
+        speech.style.display = "block";
+        speech.style.animation = "fadeIn 1s linear";
+        speech.style.opacity = "1";
+        txt.style.opacity = "1";
+        txt.style.animation = "fadeIn 2s linear";
+        setTimeout(function() {
+            txt.style.display = "none";
+        }, 2000);
+    }
+
+    function speech2(){
+        
+    }
 });
